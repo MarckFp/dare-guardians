d63eb0084eea4e421e04462a30c75ef60244085f
Marcos Fernández Pérez <33698618+MarckFp@users.noreply.github.com>
2023-08-04 09:00:30 +0000

    Check user not exists and added lint on push. Also added try catch to capture exceptions

--- commands/join.py
+++ commands/join.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+import botocore
 
 def join_game(chat_id, message, first_name, last_name, username):
     print("Executed Join command")
@@ -12,34 +13,39 @@ def join_game(chat_id, message, first_name, last_name, username):
         return "Porfavor introduce una contraseña o grupo"
     input_password = message.split(" ")[1]
     if input_password == GAME_PASSWORD:
-        client.put_item(
-            TableName=DYNAMO_TABLE,
-            Item={
-                'chat_id': {
-                    'S': str(chat_id)
+        try:
+            client.put_item(
+                TableName=DYNAMO_TABLE,
+                Item={
+                    'chat_id': {
+                        'S': str(chat_id)
+                    },
+                    'username': {
+                        'S': username
+                    },
+                    'first_name': {
+                        'S': first_name
+                    },
+                    'last_name': {
+                        'S': last_name
+                    },
+                    'role': {
+                        'S': 'None'
+                    },
+                    'attempts': {
+                        'N': '0'
+                    },
+                    'score': {
+                        'N': '0'
+                    },
+                    'group': {
+                        'S': GAME_PASSWORD
+                    }
                 },
-                'username': {
-                    'S': username
-                },
-                'first_name': {
-                    'S': first_name
-                },
-                'last_name': {
-                    'S': last_name
-                },
-                'role': {
-                    'S': 'None'
-                },
-                'attempts': {
-                    'N': '0'
-                },
-                'score': {
-                    'N': '0'
-                },
-                'group': {
-                    'S': GAME_PASSWORD
-                }
-            }
-        )
-        response = f"¡Has sido registrado correctamente en el juego {first_name}! La semana que viene empezará tu primer reto, juega limpio y diviertete =)"
+                ConditionExpression='attribute_not_exists(chat_id)'
+            )
+            response = f"¡Has sido registrado correctamente en el juego {first_name}! La semana que viene empezará tu primer reto, juega limpio y diviertete =)"
+        except botocore.exceptions.ClientError as error:
+            if error.response['Error']['Code'] != 'ConditionalCheckFailedException':
+                raise
     return response
